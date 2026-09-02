b3f790b990109eac21886ac194cf4c5f0307cf11
TH Faysal <thfaysal711@gmail.com>
2026-09-02 18:18:33 +0600

    some bug are fixed

--- src/main/java/com/example/bakir_khata/config/WebConfig.java
+++ src/main/java/com/example/bakir_khata/config/WebConfig.java
@@ -1,7 +1,18 @@
 package com.example.bakir_khata.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final TabRedirectInterceptor tabRedirectInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tabRedirectInterceptor);
+    }
 }
